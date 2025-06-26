a89aa0743b36f3380a80c440e34084dee8788d70
Phuoc <chemlink2006@gmail.com>
2025-06-26 16:43:13 +0700

    add method charm to WW

--- OOP/witch.py
+++ OOP/witch.py
@@ -1,7 +1,7 @@
 class WW:
     #this is not exactly constructor because it follows constructor named __new__
     #and object is created when __new__ is done. so it may be too late to return none in __init
-    def __init__(self, name, house="Gryffindor"): #init for initialize
+    def __init__(self, name, house="Gryffindor", spell = None): #init for initialize
         #Those lines do not work
         #if not name:
         #   return None
@@ -12,18 +12,32 @@ class WW:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.spell = spell
+        #overloaded method
     def __str__(self):
-        return f"{self.name} from {self.house}"
+        return f"{self.name} from {self.house}\n{self.name}'s spell is {self.spell}"
+    
+    #This function is called method
+    def charm(self):
+        match self.spell:
+            case "Stag":
+                print("🌵")
+            case "Otter":
+                print("📷 ")
+            case "Jacole":
+                print("🛩 ")
         
 def main():
     ww = get_ww()
+    ww.charm()
     print(ww)
 
 def get_ww():
     name = input("WW's name? ")
     house = input("WW's house? ")
+    spell = input("WW's spell? ")
     if (not house):
-        return WW(name)
-    return WW(name, house)
+        return WW(name=name, spell=spell)
+    return WW(name, house, spell)
 if __name__ == "__main__":
     main()
