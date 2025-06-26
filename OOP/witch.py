4a2090633a45a014b068675463516ce40ea29a9c
Phuoc <chemlink2006@gmail.com>
2025-06-26 16:18:36 +0700

    default value for house

--- OOP/witch.py
+++ OOP/witch.py
@@ -1,14 +1,14 @@
 class WW:
     #this is not exactly constructor because it follows constructor named __new__
     #and object is created when __new__ is done. so it may be too late to return none in __init
-    def __init__(self, name, house): #init for initialize
+    def __init__(self, name, house="Gryffindor"): #init for initialize
         #Those lines do not work
         #if not name:
         #   return None
         #raise keyword is very likely throw in java
         if not name:
             raise ValueError("missing name")
-        if house not in ["Griffindor", "Hufflepuff", "Slytherin", "Ravenclaw"]:
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
@@ -19,6 +19,8 @@ def main():
 def get_ww():
     name = input("WW's name? ")
     house = input("WW's house? ")
+    if (not house):
+        return WW(name)
     return WW(name, house)
 if __name__ == "__main__":
     main()
