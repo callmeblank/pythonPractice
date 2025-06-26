f084f7a1366a7947848f294dfbde0636437e2bcc
Phuoc <chemlink2006@gmail.com>
2025-06-26 15:22:42 +0700

    return a dict as an object and it has an advantage that avoid the need to remember location

--- OOP/witch.py
+++ OOP/witch.py
@@ -1,9 +1,11 @@
 def main():
     witch = get_witch()
-    print(f"Name == {witch[0]}\nHouse == {witch[1]}")
+    print(f"Name == {witch["name"]}\nHouse == {witch["house"]}")
 
 def get_witch():
-    return [input("Witch's name? "), input("Witch's house? ")] #This line makes this program looks like "OOP" with (tuple, list, dict, etc) as an object (even we can return a dict)
-
+    name = input("Witch's name? ")
+    house = input("Witch's house? ")
+    return {"name" : name, "house": house} #This line makes this program looks like "OOP" with (tuple, list, dict, etc) as an object (even we can return a dict)
+    #return a dict can have an advantage that you don't need to remember the location of keys anymore
 if __name__ == "__main__":
     main()
