19e8627185214dad0abf141a4f9b02d52e5a1571
Phuoc <chemlink2006@gmail.com>
2025-06-26 16:04:34 +0700

    prevent from error

--- OOP/witch.py
+++ OOP/witch.py
@@ -1,5 +1,15 @@
 class WW:
-    def __init__(self, name, house): #Default constructor
+    #this is not exactly constructor because it follows constructor named __new__
+    #and object is created when __new__ is done. so it may be too late to return none in __init
+    def __init__(self, name, house): #init for initialize
+        #Those lines do not work
+        #if not name:
+        #   return None
+        #raise keyword is very likely throw in java
+        if not name:
+            raise ValueError("missing name")
+        if house not in ["Griffindor", "Hufflepuff", "Slytherin", "Ravenclaw"]:
+            raise ValueError("Invalid house")
         self.name = name
         self.house = house
 def main():
