cf3f79211edf470eae4f0e2664f883d35d23519e
Phuoc <chemlink2006@gmail.com>
2025-06-26 15:41:40 +0700

    build default constructor and declare instance variable

--- OOP/witch.py
+++ OOP/witch.py
@@ -1,11 +1,14 @@
+class WW:
+    def __init__(self, name, house): #Default constructor
+        self.name = name
+        self.house = house
 def main():
-    witch = get_witch()
-    print(f"Name == {witch["name"]}\nHouse == {witch["house"]}")
+    ww = get_ww()
+    print(f"Name == {ww.name}\nHouse == {ww.house}")
 
-def get_witch():
-    name = input("Witch's name? ")
-    house = input("Witch's house? ")
-    return {"name" : name, "house": house} #This line makes this program looks like "OOP" with (tuple, list, dict, etc) as an object (even we can return a dict)
-    #return a dict can have an advantage that you don't need to remember the location of keys anymore
+def get_ww():
+    name = input("WW's name? ")
+    house = input("WW's house? ")
+    return WW(name, house)
 if __name__ == "__main__":
     main()
